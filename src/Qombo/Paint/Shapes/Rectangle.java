e7ee40f0478e441ae5ce216f11ec88709f0fd0ee
louay-hesham <louay.hesham@gmail.com>
2016-05-13 16:36:58 +0200

    Some fixes

--- src/Qombo/Paint/Shapes/Rectangle.java
+++ src/Qombo/Paint/Shapes/Rectangle.java
@@ -11,9 +11,11 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.RenderingHints;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Path2D;
 import java.awt.geom.Point2D;
+import sun.awt.SunHints;
 
 /**
  *
